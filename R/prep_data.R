ff8c2d0dda1f31ba0a9ecae876461ece41a487f1
Luke W Johnston <lwjohnst@gmail.com>
2016-08-23 16:09:50 -0400

    Created function to clean QIC output

--- R/prep_data.R
+++ R/prep_data.R
@@ -30,3 +30,16 @@ prep_dysglycemia_data <- function(data) {
 
     return(dysgly.data)
 }
+
+
+#' Clean data output from the QIC model selection functions.
+#'
+#' @param data QIC output data.
+#'
+#' @export
+clean_qic <- function(data) {
+    data %>%
+        tibble::rownames_to_column() %>%
+        dplyr::select(Model = rowname, QIC = IC, Delta = delta) %>%
+        dplyr::mutate(id = seq_len(nrow(.)))
+}
