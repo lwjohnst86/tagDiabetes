4b7b7816fb6a7a8fc163baaf0a47789a037b5a59
Luke W Johnston <lwjohnst@gmail.com>
2016-08-24 19:06:45 -0400

    Added more and completed the pls functions.

--- R/pls.R
+++ R/pls.R
@@ -2,6 +2,13 @@
 #
 # Grab or combine data ----------------------------------------------------
 
+#' Prepare the project data for PLS analysis.
+#'
+#' @param data Project data.
+#' @param y Y or outcome variable.
+#' @param x X or exposure variables of interest.
+#'
+#' @export
 prep_pls_data <- function(data, y, x) {
     data %>%
         dplyr::filter(VN == 0) %>%
@@ -11,15 +18,50 @@ prep_pls_data <- function(data, y, x) {
 
 # Analyze -----------------------------------------------------------------
 
+#' Compute PLS analysis.
+#'
+#' @param data Project data.
+#' @param y Outcome variable.
+#' @param x Exposure variables.
+#' @param ncomp Number of components.
+#'
+#' @export
 analyze_pls <- function(data = project_data,
-                        y, x = tg_pct, ncomp = 2,
-                        type = c('train', 'test', 'full')) {
+                        y, x = tg_pct, ncomp = NULL) {
     data %>%
         prep_pls_data(y = y, x = x) %>%
         mason::design('pls') %>%
-        mason::add_settings(ncomp = ncomp, cv.data = TRUE, cv.seed = 12345) %>%
+        mason::add_settings(ncomp = ncomp, validation = 'CV', cv.data = TRUE, cv.seed = 5436) %>%
         mason::add_variables('yvars', y) %>%
         mason::add_variables('xvars', x) %>%
-        mason::construct(cv = type) %>%
+        mason::construct() %>%
         mason::scrub()
+}
+
+# Plots -------------------------------------------------------------------
+
+#' Plot the X loadings of the PLS results.
+#'
+#' @param data PLS output results.
+#'
+#' @export
+plot_pls <- function(data) {
+    seer::view_pls_xloadings(data, renaming.x = renaming_fats) +
+        graph_theme(minor.grid.lines = FALSE)
+}
+
+# Calculations ------------------------------------------------------------
+
+#' Calculate the correlation between the predicted and measured outcome.
+#'
+#' @param model The PLS output results.
+#' @param test The test data to predict from.
+#' @param ncomps The component to predict on.
+#'
+#' @export
+calc_pred_corr <- function(model, test, ncomps = 1) {
+    predicted <- predict(model, ncomp = ncomps, newdata = test)
+    measured <- as.matrix(model.response(model.frame(formula(model),
+                                                     data = test)))
+    cor(predicted, measured)
 }
