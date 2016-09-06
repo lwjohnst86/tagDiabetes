5a7e8d9a13a4fc21c5aeb6fccc98a46d0cbfbded
Luke W Johnston <lwjohnst@gmail.com>
2016-09-06 13:43:00 -0400

    Added function to make QIC table in manuscript

--- R/tables.R
+++ R/tables.R
@@ -70,3 +70,103 @@ table_qic <- function(data, caption = NULL) {
                       Delta = format_rounding(Delta, 1)) %>%
         pander::pander(missing = '', caption = caption)
 }
+
+#' QIC table for manuscript.
+#'
+#' @param data Project data.
+#' @param caption Caption for table.
+#'
+#' @export
+table_qic_main <- function(data = project_data, caption = NULL) {
+    prep_qic <- data %>%
+        prep_gee_data() %>%
+        dplyr::select(
+            SID,
+            VN,
+            lISSI2,
+            lISI,
+            TotalTG,
+            TotalNE,
+            Sex,
+            Ethnicity,
+            MET,
+            ALT,
+            BaseAge,
+            Waist,
+            BMI,
+            AlcoholPerWk,
+            TobaccoUse,
+            FamHistDiab
+        ) %>%
+        stats::na.omit()
+
+    qic_is <- prep_qic %>%
+        dplyr::select(-lISSI2)
+
+    qic_bcf <- prep_qic %>%
+        dplyr::select(-lISI)
+    M0_ar_is <- geepack::geeglm(
+        lISI ~ TotalTG + VN,
+        data = qic_is,
+        id = SID,
+        family = stats::gaussian,
+        corstr = 'ar1'
+    )
+    M0 <- M0_ar_is
+    Full <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex + ALT + MET +
+                AlcoholPerWk + FamHistDiab + TobaccoUse + TotalNE)
+    M1 <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex)
+    M2 <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex + ALT)
+    M3 <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex + ALT + MET)
+    M4 <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex + ALT + MET + AlcoholPerWk)
+    M5 <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex + ALT + MET + AlcoholPerWk +
+                          FamHistDiab)
+    M6 <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex + ALT + FamHistDiab + TobaccoUse)
+    M7 <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex + ALT + FamHistDiab)
+    M8 <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex + ALT + MET + FamHistDiab)
+    M9 <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex + ALT + MET + TotalNE)
+    M10 <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex + ALT + MET + TotalNE +
+                          FamHistDiab)
+
+    mod_is <-
+        MuMIn::model.sel(M0, Full, M1, M2, M3, M4, M5, M6, M7, M8, M9, M10,
+                         rank = MuMIn::QIC) %>%
+        clean_qic() %>%
+        dplyr::bind_rows(data.frame(Model = '**log(ISI)**', id = 0.5))
+
+    M0_ar_bcf <- geepack::geeglm(
+        lISSI2 ~ TotalTG + VN,
+        data = qic_bcf,
+        id = SID,
+        family = stats::gaussian,
+        corstr = 'ar1'
+    )
+    M0 <- M0_ar_bcf
+    Full <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex + ALT + MET +
+                AlcoholPerWk + FamHistDiab + TobaccoUse + TotalNE)
+    M1 <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex)
+    M2 <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex + ALT)
+    M3 <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex + ALT + MET)
+    M4 <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex + ALT + MET + AlcoholPerWk)
+    M5 <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex + ALT + MET + AlcoholPerWk +
+                          FamHistDiab)
+    M6 <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex + ALT + FamHistDiab + TobaccoUse)
+    M7 <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex + ALT + FamHistDiab)
+    M8 <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex + ALT + MET + FamHistDiab)
+    M9 <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex + ALT + MET + TotalNE)
+    M10 <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex + ALT + MET + TotalNE +
+                          FamHistDiab)
+
+    mod_bcf <-
+        MuMIn::model.sel(M0, Full, M1, M2, M3, M4, M5, M6, M7, M8, M9, M10,
+                         rank = MuMIn::QIC) %>%
+        clean_qic() %>%
+        dplyr::mutate(id = max(id) + id) %>%
+        {
+            dplyr::bind_rows(., data.frame(Model = '**log(ISSI-2)**', id = min(.['id']) - 0.5))
+        }
+
+    dplyr::bind_rows(mod_is, mod_bcf) %>%
+        table_qic(caption = caption)
+
+}
