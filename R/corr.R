6de362595c53fbcc0212158cf72d84f3c03a8811
Luke W Johnston <lwjohnst@gmail.com>
2017-01-05 21:09:10 -0500

    Created and added heatmap fig (with just TAGFA)

--- R/corr.R
+++ R/corr.R
@@ -39,6 +39,30 @@ analyze_corr <-
                       Correlations = round(Correlations, 2))
     }
 
+#' Correlation analysis of just TAGFA.
+#'
+#' @param data Project data
+#' @param x The TAGFA
+#'
+#' @export
+analyze_corr_tagfa <-
+    function(data = project_data,
+             x = tg_pct) {
+
+    data %>%
+        dplyr::filter(VN == 0) %>%
+        mason::design('cor') %>%
+        mason::add_settings(method = 'pearson', use = 'complete.obs', hclust.order = TRUE) %>%
+        mason::add_variables('xvars', x) %>%
+        mason::construct() %>%
+        mason::scrub() %>%
+        mason::polish_renaming(renaming_fats, 'Vars2') %>%
+        mason::polish_renaming(renaming_fats, 'Vars1') %>%
+        dplyr::mutate(Vars2 = factor(Vars2, unique(Vars2)),
+                      Vars1 = factor(Vars1, unique(Vars1)))
+    }
+
+
 # Plotting ----------------------------------------------------------------
 
 #' Correlation heatmap plot.
@@ -46,13 +70,14 @@ analyze_corr <-
 #' @param results Correlation results
 #'
 #' @export
-plot_heatmap <- function(results) {
+plot_heatmap <- function(results, text = TRUE) {
      results %>%
         seer::view_heatmap(
             y = 'Vars2',
             x = 'Vars1',
             ylab = 'Triacylglycerol fatty acids (nmol/mL)',
             number.colours = 5,
+            values.text = text,
             values.size = 4) +
         graph_theme(ticks = FALSE, legend.pos = 'right') +
         ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, vjust = 1, hjust = 1))
