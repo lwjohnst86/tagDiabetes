b6ff82d30fcdbe687bac56f2b571d301b60161ad
Luke W Johnston <lwjohnst@gmail.com>
2017-06-15 13:45:47 -0400

    Fix pander issue with tbl_df

--- R/gee.R
+++ R/gee.R
@@ -226,6 +226,7 @@ table_gee_main <- function(results, caption = NULL, digits = 1) {
         ) %>%
         dplyr::select(-unit) %>%
         dplyr::rename('Fatty acid' = Xterms) %>%
+        as.data.frame() %>%
         pander::pander(missing = '', caption = caption)
 }
 
