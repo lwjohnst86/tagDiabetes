cf8aafe6580a900561e2763ef339924c1acb3749
Luke W Johnston <lwjohnst@gmail.com>
2018-01-23 14:29:30 -0500

    remove dplyr::, tidyr::, mason::

--- R/gee.R
+++ R/gee.R
@@ -8,32 +8,32 @@
 #' @export
 prep_gee_data <- function(data) {
     data <- data %>%
-        dplyr::filter(DM != 1)
+        filter(DM != 1)
 
     no_fattyacids <- data %>%
-        dplyr::select(-dplyr::matches('pct_tg\\d+|^tg\\d+'),
+        select(-matches('pct_tg\\d+|^tg\\d+'),
                       -TotalNE,-TotalTG, -BaseTAG, -lBaseTAG)
 
     scaled_variables <- data %>%
-        dplyr::filter(VN == 0) %>%
-        dplyr::select(SID, TotalNE, TotalTG, BaseTAG, lBaseTAG,
-                      dplyr::matches('pct_tg\\d+|^tg\\d+')) %>%
-        dplyr::mutate_at(dplyr::vars(-SID), dplyr::funs(as.numeric(scale(.))))
+        filter(VN == 0) %>%
+        select(SID, TotalNE, TotalTG, BaseTAG, lBaseTAG,
+                      matches('pct_tg\\d+|^tg\\d+')) %>%
+        mutate_at(vars(-SID), funs(as.numeric(scale(.))))
 
-    dplyr::full_join(
+    full_join(
             no_fattyacids,
             scaled_variables,
             by = 'SID'
         ) %>%
-        dplyr::group_by(VN) %>%
-        dplyr::mutate(
+        group_by(VN) %>%
+        mutate(
             Waist = as.numeric(scale(Waist)),
             ALT = as.numeric(scale(ALT)),
             BaseAge = as.numeric(scale(BaseAge)),
             MET = as.numeric(scale(MET))
         ) %>%
-        dplyr::ungroup() %>%
-        dplyr::arrange(SID, VN)
+        ungroup() %>%
+        arrange(SID, VN)
 }
 
 #' Add the extracted PLS scores to the data for use in GEE.
@@ -43,23 +43,23 @@ prep_gee_data <- function(data) {
 #' @export
 prep_gee_pls_data <- function(data = project_data, pls_results) {
     sid_vn <- data %>%
-        dplyr::filter(VN == 0) %>%
-        dplyr::select_(.dots = c(outcomes, tg_pct, 'SID', covariates)) %>%
+        filter(VN == 0) %>%
+        select_(.dots = c(outcomes, tg_pct, 'SID', covariates)) %>%
         stats::na.omit() %>%
-        dplyr::full_join(dplyr::select(project_data, SID, VN)) %>%
-        dplyr::select_(.dots = c(outcomes, 'SID', 'VN'))
+        full_join(select(project_data, SID, VN)) %>%
+        select_(.dots = c(outcomes, 'SID', 'VN'))
 
     pls_results %>%
     {
-        dplyr::bind_cols(tibble::as_data_frame(.$model$Y),
+        bind_cols(tibble::as_data_frame(.$model$Y),
                          tibble::as_data_frame(unclass(.$scores))[1:2])
     } %>%
-        dplyr::rename(Comp1 = `Comp 1`, Comp2 = `Comp 2`) %>%
-        dplyr::full_join(sid_vn) %>%
-        dplyr::select(SID, VN, Comp1, Comp2) %>%
-        dplyr::full_join(data) %>%
-        dplyr::arrange(SID, VN) %>%
-        tidyr::fill(Comp1, Comp2)
+        rename(Comp1 = `Comp 1`, Comp2 = `Comp 2`) %>%
+        full_join(sid_vn) %>%
+        select(SID, VN, Comp1, Comp2) %>%
+        full_join(data) %>%
+        arrange(SID, VN) %>%
+        fill(Comp1, Comp2)
 }
 
 # Analyze -----------------------------------------------------------------
@@ -107,30 +107,30 @@ analyze_gee <- function(data = project_data,
            pls = {data <- prep_gee_pls_data(data, pls_results = pls_results)})
 
     data %>%
-        mason::design('gee') %>%
-        mason::add_settings(family = stats::gaussian(),
+        design('gee') %>%
+        add_settings(family = stats::gaussian(),
                             corstr = 'ar1', cluster.id = 'SID') %>%
-        mason::add_variables('yvars', y) %>%
+        add_variables('yvars', y) %>%
         {if (is.list(x)) {
-            mason::add_variables(., 'xvars', x[['tg_pct']]) %>%
-                mason::add_variables('covariates', covars) %>% {
+            add_variables(., 'xvars', x[['tg_pct']]) %>%
+                add_variables('covariates', covars) %>% {
                     if (int) {
-                        mason::add_variables(., 'interaction', intvar)
+                        add_variables(., 'interaction', intvar)
                     } else {
                         .
                     }
                 } %>%
-                mason::construct() %>%
-                mason::add_variables('xvars', x[['tg_conc']]) %>%
-                mason::construct() %>%
-                mason::add_variables('xvars', x[['tg_total']]) %>%
-                mason::construct()
+                construct() %>%
+                add_variables('xvars', x[['tg_conc']]) %>%
+                construct() %>%
+                add_variables('xvars', x[['tg_total']]) %>%
+                construct()
         } else {
-            mason::add_variables(., 'xvars', x) %>%
-                mason::construct()
+            add_variables(., 'xvars', x) %>%
+                construct()
         }} %>%
-        mason::scrub() %>%
-        mason::polish_filter(extract_term, 'term') %>%
+        scrub() %>%
+        polish_filter(extract_term, 'term') %>%
         categorize_fa_model_output() %>%
         transform_continuous_model_output() %>%
         rename_terms_model_output(rename_x = rename_x, rename_y = rename_y) %>%
@@ -142,7 +142,7 @@ analyze_gee <- function(data = project_data,
 
 categorize_fa_model_output <- function(gee_results) {
     gee_results %>%
-        dplyr::mutate(unit = ifelse(
+        mutate(unit = ifelse(
             grepl('pct', Xterms),
             'mol%',
             ifelse(grepl('^tg\\d', Xterms), 'nmol/mL',
@@ -152,38 +152,38 @@ categorize_fa_model_output <- function(gee_results) {
 
 transform_continuous_model_output <- function(gee_results) {
     gee_results %>%
-        mason::polish_transform_estimates(function(x) (exp(x) - 1) * 100)
+        polish_transform_estimates(function(x) (exp(x) - 1) * 100)
 }
 
 rename_terms_model_output <- function(gee_results, rename_x, rename_y) {
     gee_results %>%
-        dplyr::mutate_at('Xterms', dplyr::funs(rename_x)) %>%
-        dplyr::mutate_at('Yterms', dplyr::funs(rename_y))
+        mutate_at('Xterms', funs(rename_x)) %>%
+        mutate_at('Yterms', funs(rename_y))
 }
 
 padjust_model_output <- function(gee_results) {
     gee_results %>%
-        mason::polish_adjust_pvalue(method = 'BH') %>%
-        dplyr::rename(unadj.p.value = p.value, p.value = adj.p.value)
+        polish_adjust_pvalue(method = 'BH') %>%
+        rename(unadj.p.value = p.value, p.value = adj.p.value)
 }
 
 order_model_output <- function(gee_results) {
     gee_results %>%
-        dplyr::mutate(
+        mutate(
             order1 = substr(Xterms, nchar(Xterms), nchar(Xterms)),
             order1 = ifelse(order1 == 0, 10, order1),
             order1 = ifelse(order1 == 'l', 20, order1),
             order1 = ifelse(order1 == 'G', 30, order1),
             order1 = as.integer(order1)
         ) %>%
-        dplyr::arrange(desc(order1)) %>%
-        dplyr::mutate(Xterms = factor(Xterms, unique(Xterms))) %>%
-        dplyr::select(-order1)
+        arrange(desc(order1)) %>%
+        mutate(Xterms = factor(Xterms, unique(Xterms))) %>%
+        select(-order1)
 }
 
 set_outcome_order_model_output <- function(gee_results) {
     gee_results %>%
-        dplyr::mutate(Yterms = factor(
+        mutate(Yterms = factor(
             Yterms,
             levels = c('log(HOMA2-IR)', 'log(HOMA2-%S)', 'log(ISI)',
                        'log(IGI/IR)', 'log(ISSI-2)'),
@@ -203,29 +203,29 @@ set_outcome_order_model_output <- function(gee_results) {
 #' @export
 table_gee_main <- function(results, caption = NULL, digits = 1) {
     table_data <- results %>%
-        dplyr::mutate_at(
-            dplyr::vars(estimate, conf.low, conf.high),
-            dplyr::funs(format_rounding(., digits = digits))
+        mutate_at(
+            vars(estimate, conf.low, conf.high),
+            funs(format_rounding(., digits = digits))
         ) %>%
-        dplyr::mutate(
+        mutate(
             p.binary = ifelse(p.value <= 0.05, '\\*', ''),
             estimate.ci = paste0(estimate, ' (', conf.low, ', ', conf.high, ')',
                                  p.binary)
         ) %>%
-        dplyr::select(Yterms, Xterms, unit, estimate.ci) %>%
-        tidyr::spread(Yterms, estimate.ci) %>%
-        dplyr::mutate(Xterms = as.character(Xterms))
+        select(Yterms, Xterms, unit, estimate.ci) %>%
+        spread(Yterms, estimate.ci) %>%
+        mutate(Xterms = as.character(Xterms))
 
-    dplyr::bind_rows(
+    bind_rows(
             tibble::data_frame(Xterms = paste0('**Totals**')),
-            dplyr::filter(table_data, unit == 'Totals'),
+            filter(table_data, unit == 'Totals'),
             tibble::data_frame(Xterms = paste0('**nmol/mL**')),
-            dplyr::filter(table_data, unit == 'nmol/mL'),
+            filter(table_data, unit == 'nmol/mL'),
             tibble::data_frame(Xterms = paste0('**mol%**')),
-            dplyr::filter(table_data, unit == 'mol%')
+            filter(table_data, unit == 'mol%')
         ) %>%
-        dplyr::select(-unit) %>%
-        dplyr::rename('Fatty acid' = Xterms) %>%
+        select(-unit) %>%
+        rename('Fatty acid' = Xterms) %>%
         as.data.frame() %>%
         pander::pander(missing = '', caption = caption)
 }
