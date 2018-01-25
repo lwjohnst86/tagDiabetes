27acc74bf123437b27b23f4c33b63960a371aad2
Luke W Johnston <lwjohnst@gmail.com>
2018-01-24 22:25:50 -0500

    renaming fats fix (with promise.misc fun)

--- R/renaming.R
+++ R/renaming.R
@@ -33,21 +33,13 @@ renaming_outcomes <- function(x) {
 
 renaming_fats <- function(x) {
     x %>%
-        gsub('.*(\\d\\d)(\\d)', '\\1:\\2', .) %>%
-        gsub('n(\\d)$', 'n-\\1', .) %>%
-        gsub('D(\\d\\d)$', 'D-\\1', .) %>%
-        gsub('^pct_', '', .) %>%
-        gsub('TotalTG', 'Total', .) %>%
-        gsub('^TAG$', 'Clinical TAG', .)
+        stringr::str_replace('^pct_', '') %>%
+        stringr::str_replace('TotalTG', 'Total') %>%
+        stringr::str_replace('BaseTAG', 'Clinical TAG') %>%
+        PROMISE.misc::renaming_fa(keep.fraction = FALSE)
 }
 
 renaming_fraction <- function(x) {
     x %>%
         gsub('tg', 'Triacylglycerol', .)
-}
-
-renaming_list <- function(x) {
-    x %>%
-        renaming_fa() %>%
-        renaming_outcomes()
 }
